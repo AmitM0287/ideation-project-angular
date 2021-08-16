5089948e0ba070ab804c3abc31c2b90d873bb228
AmitM0287 <amitmanna0287@gmail.com>
2021-08-16 11:23:29 +0530

    refactor: home component updated

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: UserService) { }
 
   ngOnInit(): void {
+  }
+
+  name = new FormControl('', [Validators.required, Validators.pattern('[A-Za-z ]{4,}')]);
+  phone = new FormControl('', [Validators.required, Validators.pattern('[0-9]{10}')]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  propertyType = new FormControl('', [Validators.required]);
+  serviceType = new FormControl('', [Validators.required]);
+  message = new FormControl('', [Validators.required, Validators.pattern('[A-Za-z ]{4,}')]);
+  
+  submit() {
+    console.log(this.name.value, this.propertyType.value, this.message.value)
   }
 
 }
